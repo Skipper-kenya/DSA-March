0c1b0b53f90346e77690c051cb4428c8afeeab87
Skipper-Kenya <kimwetichandrew@gmail.com>
2025-03-27 20:12:58 +0300

    Arrays

---
+++ Arrays/Arrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTA.Arrays
+{
+    public class Arrays
+    {
+        public void insertAtEndOfArray()
+        {
+            int[] myArray = new int[5];
+
+            int length = 0;
+
+            for(int i=0; i<5;i++)
+            {
+                myArray[i] = i;
+            }
+
+
+            //inserting a new element at the end of the array
+
+        }
+    } }
--- Program.cs
+++ Program.cs
@@ -1,25 +1,7 @@
 ﻿using DTA.LinkedList;
+using DTA.Arrays;
 
 LinkedList linkedList = new LinkedList();
+Arrays myArray = new Arrays();
 
-linkedList.insertFirst(3);
-linkedList.insertFirst(5);
-linkedList.insertFirst(7);
-linkedList.insertFirst(9);
-linkedList.insertFirst(10);
-
-linkedList.DisplayList();
-
-Console.WriteLine("after deleting two nodes");
-
-linkedList.DeleteFirst();
-linkedList.DeleteFirst();
-
-
-linkedList.DisplayList();
-
-Console.WriteLine("after inserting at the last");
-
-linkedList.InsertLast(111);
-
-linkedList.DisplayList();
+myArray.insertAtEndOfArray();
